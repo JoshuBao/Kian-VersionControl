47bfa40d71e7b530822121b0da2c6e0c13d94e2e
Joshua Cheng <joshuacheng888@gmail.com>
2022-09-22 23:52:12 -0700

    Added randomTextFile name placeholder into TreeObject

--- src/blobindex/Tree.java
+++ src/blobindex/Tree.java
@@ -1,6 +1,7 @@
 package blobindex;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -12,7 +13,7 @@ public class Tree {
 	public Tree (ArrayList<String> keyVal) throws FileNotFoundException {
 		content="";
 		for (String obj: keyVal) {
-			content=content+obj+"\n";
+			content=content+obj+ " : " + "randomName.txt" + "\n";
 		}
 		content = content.substring(0, content.length()-1);
 		String sha1 = encrypt(content);
@@ -32,5 +33,22 @@ public class Tree {
 			e.printStackTrace();
 		}
 		return sha1;
+	}
+
+
+	public static void main (String [] args) throws IOException {
+		Index git = new Index();
+		git.init();
+		git.add("foo.txt");
+		git.add("bar.txt");
+		git.add("foobar.txt");
+		
+		ArrayList<String> treeList = new ArrayList<String>();
+		treeList.add("blob : b66399e65f956699e7ece173e73ab2b4021ff1ab");
+		treeList.add("blob : 27d7b0b544c4f31c9051db01c1554b0a544a54e6");
+		treeList.add("blob : 3f0b692b963a9a3cc402e74f8b4374538b1125f7");
+		
+		Tree tree1 = new Tree(treeList);
+	
 	}
 }
